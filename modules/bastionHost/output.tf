626345e780a6d897c95be666be09e9276d1328a6
Krzysztof Sobieraj <Krzysztof.Sobieraj@StepStone.com>
2023-01-19 22:54:28 +0100

    #7: outputs cleanup

--- modules/bastionHost/output.tf
+++ modules/bastionHost/output.tf
@@ -1,7 +1,3 @@
-output "instance_ami" {
-  description = "Bastion Host AMI"
-  value       = aws_instance.bastion_host.ami
-}
 output "instance_id" {
   description = "Bastion Host instance ID"
   value       = aws_instance.bastion_host.id
--- modules/webserver/output.tf
+++ modules/webserver/output.tf
@@ -1,12 +1,6 @@
-output "instance_ami" {
-  description = "webserver AMI"
-  value       = aws_instance.webserver.ami
-}
-
 output "instance_id" {
   value = aws_instance.webserver.id
 }
-
-output "public_ip" {
-  value = aws_instance.webserver.public_ip
+output "ip" {
+  value = aws_instance.webserver.private_ip
 }
